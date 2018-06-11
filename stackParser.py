9eee37f8d8b7028d8404ef35f79d852a4e0b4930
RWare12 <39044631+RWare12@users.noreply.github.com>
2018-06-11 17:44:38 +0800

    Update stackParser.py

--- stackParser.py
+++ stackParser.py
@@ -5,6 +5,9 @@ userList1 = [] #getting username from `windowbg` tag
 userList2 = [] #getting username from `windowbg2` tag
 message = [] #getting the post from users
 quotes = [] #getting quotes from user if there is
+quotesUserID = [] #user for being quoted
+dataAndtime = [] #date and time of post
+finalOuput = [] #will represent JSON of data scraped
 
 page = req.urlopen('http://forums.abs-cbn.com/bagani-(march-5-2018-present)/alamat/')
 soup = BeautifulSoup(page, 'html.parser') # from url
@@ -22,10 +25,42 @@ for post in soup.find_all('div',{'class':'post'}):
 for post in soup.find_all('div',{'class':'post'}):
     if post.blockquote is not None:
         quotes.append(post.blockquote.text.strip())
+        quotesUserID.append(post.a.text)
     else:
         quotes.append("None")
+        quotesUserID.append("None")
 
-print("userList1: ",userList1) 
-print("userList2: ",userList2) 
-print("message: ",message) 
-print("quotes: ", quotes)
+for post in soup.find_all('div',{'class':'smalltext'}):
+    dataAndtime.append(post.text.strip())
+
+
+
+print(len(message))
+
+ctr = 0 #counter for messages to the post
+
+for user in range(len(userList1)):
+    mainAnswer = {}
+    quotesInfo = {}
+    mainAnswer['user_id'] = userList1[user]
+    # mainAnswer['message'] = message[ctr]
+    mainAnswer['quotes'] = quotesInfo
+    quotesInfo['user_id'] = quotesUserID[ctr]
+    ctr += 1
+    finalOuput.append(mainAnswer)
+    if user < len(userList1)-1:
+        mainAnswer = {}
+        mainAnswer['user_id'] = userList2[user]
+        # mainAnswer['message'] = message[ctr]
+        mainAnswer['quotes'] = quotesInfo
+        quotesInfo['user_id'] = quotesUserID[ctr]
+        ctr += 1
+        finalOuput.append(mainAnswer)
+
+
+
+# print("quotes: ",quotes)
+# print("quotesUserID: ",quotesUserID)
+# print("dateAndtime: ",dataAndtime)
+
+print(finalOuput) #final
