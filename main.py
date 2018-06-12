edd97be8d38472f3043fb35cab39f37f922dd7d2
RWare12 <39044631+RWare12@users.noreply.github.com>
2018-06-12 22:21:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 import stackParser
 import saveJsonFile
+import re
 
 URL = ''
 
 while(URL is not 'q'):
     print('Enter \'q\' to exit!')
     URL = input('ENTER URL: ') #getting URL
-    if "https://stackoverflow.com/questions/" in URL:
+
+    inputURL = re.findall('https?://forums.abs-cbn.com+/(?:[-\w.!()]|(?:%[.]))+/(?:[-\w.!()]|(?:%[.]))+', URL)
+
+    if len(inputURL) is not 0:
             result = stackParser.parse(URL)
             saveJsonFile.saveFile(result)
+
+    elif URL is 'q':
+        exit()
+
     else:
             print("Invalid URL!")
 
-exit()
